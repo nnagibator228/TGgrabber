db7ec06fbbf91e1d79167ba35f1c4aac7df632a9
plzdontcry <plzd@ntcry.ru>
2022-06-08 13:13:05 +0300

    bugs fixed #1

--- admin_panel_service/sql.py
+++ admin_panel_service/sql.py
@@ -26,45 +26,53 @@ class SQL:
             port=self.port)
         return connection
 
+    def reconnect(self):
+        self.connection.ping(reconnect=True)
+
     def create_table_config(self):
-        """Создание таблицы для хранения каналов"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS config(donor varchar(255), moder varchar("
                          "255), channel varchar(255))")
         return self.connection.commit()
 
     def create_table_database(self):
-        """Создание таблицы для хранения информации о постах"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS DataBase1(id MEDIUMINT UNSIGNED ZEROFILL AUTO_INCREMENT, username "
                  "varchar(255), message_id varchar(255), PRIMARY KEY (id))")
         return self.connection.commit()
 
     def message_id_exists(self, username, message_id: int):
-        """Проверка есть ли message_id в БД"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT * FROM DataBase1 WHERE username=%s AND message_id=%s", (username, message_id,))
         result = self.cur.fetchall()
         return bool(len(result))
 
     def add_message_id(self, username, message_id):
-        """Добавление message_id"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("INSERT INTO DataBase1 VALUES (NULL,%s,%s)", (username, message_id,))
         return self.connection.commit()
 
     def get_last_rowid(self):
+        self.reconnect()
         self.cur = self.connection.cursor()
-        return self.cur.execute("SELECT LAST_INSERT_ID()")
+        self.cur.execute("SELECT LAST_INSERT_ID()")
+        return self.cur.fetchone()
 
     def get_data_in_table(self, message):
         """Получение записи о посте в таблице"""
+        self.reconnect()
         self.cur = self.connection.cursor()
-        return self.cur.execute(f"SELECT * FROM DataBase1 WHERE id = {message.text}")
+        self.cur.execute(f"SELECT * FROM DataBase1 WHERE id = {message.text}")
+        return self.cur.fetchone()
 
     def add_donor(self, name: str):
         """Добавление канала донора"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT donor FROM config WHERE donor=%s", (name,))
         result = self.cur.fetchall()
@@ -77,6 +85,7 @@ class SQL:
     def delete_donor(self, name: str):
         """Удаление канала донора"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT donor FROM config WHERE donor = %s", (name,))
         result = self.cur.fetchall()
@@ -90,6 +99,7 @@ class SQL:
     def add_moder(self, name: str):
         """Добавление канала модерации"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT moder FROM config WHERE moder=%s", (name,))
         result = self.cur.fetchall()
@@ -102,6 +112,7 @@ class SQL:
     def delete_moder(self, name: str):
         """Удаление модера"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT moder FROM config WHERE moder = %s", (name,))
         result = self.cur.fetchall()
@@ -115,6 +126,7 @@ class SQL:
     def add_channel(self, name: str):
         """Добавление основного канала"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT channel FROM config WHERE channel=%s", (name,))
         result = self.cur.fetchall()
@@ -127,6 +139,7 @@ class SQL:
     def delete_channel(self, name: str):
         """Удаление основного канала"""
         # Проверка на наличие записи такого канала
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT channel FROM config WHERE channel = %s", (name,))
         result = self.cur.fetchall()
@@ -139,6 +152,7 @@ class SQL:
 
     def get_donor(self):
         """Возвращает список с каналами донорами"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         donor_list = []
         self.cur.execute("SELECT donor FROM config")
@@ -152,6 +166,7 @@ class SQL:
 
     def get_moder(self):
         """Возвращает канал модерации"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         moder = None
         self.cur.execute("SELECT moder FROM config")
@@ -165,6 +180,7 @@ class SQL:
 
     def get_channel(self):
         """Возвращает основной канал"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         self.cur.execute("SELECT channel FROM config")
         result = self.cur.fetchall()
@@ -177,6 +193,7 @@ class SQL:
 
     def print_donor(self):
         """Возвращает список каналов доноров"""
+        self.reconnect()
         self.cur = self.connection.cursor()
         donor_list = []
         self.cur.execute("SELECT donor FROM config")
